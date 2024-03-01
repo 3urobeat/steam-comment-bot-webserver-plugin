7e4fa886f0f9d4cbc8df5e55584eb671d95aa99c
3urobeat <35304405+3urobeat@users.noreply.github.com>
2024-03-01 22:17:00 +0100

    chore: Update runCommand() call

--- plugin.js
+++ plugin.js
@@ -4,10 +4,10 @@
  * Created Date: 25.02.2022 14:12:17
  * Author: 3urobeat
  *
- * Last Modified: 30.06.2023 10:19:09
+ * Last Modified: 2024-03-01 22:11:25
  * Modified By: 3urobeat
  *
- * Copyright (c) 2022 3urobeat <https://github.com/3urobeat>
+ * Copyright (c) 2022 - 2024 3urobeat <https://github.com/3urobeat>
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -127,7 +127,7 @@ Plugin.prototype.ready = function() {
 
 
         // Run the comment command
-        this.commandHandler.runCommand("comment", [ amount, receivingID ], requestingID, respondModule, this, { res: res });
+        this.commandHandler.runCommand("comment", [ amount, receivingID ], respondModule, this, { res: res, userID: requestingID });
     });
 
     this.app.get("/output", (req, res) => { // Show output
